edf872535ba8bc6c08e44107f806cfd2f46a466e
ylsama <ylongkaka3@gmail.com>
2022-03-23 21:14:22 +0700

    variable scope

--- ylsama/variable/main.go
+++ ylsama/variable/main.go
@@ -5,14 +5,34 @@ import (
 	"math/cmplx"
 )
 
+// example of var block placement can be outsite main func scope
 var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
+	toBe   bool       = false
+	maxInt uint64     = 1<<64 - 1
 	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
-	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
-	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	// example of var block placement can be inside main func scope
+	var (
+		explicit string = "Hello, I'm a explicitly declared variable"
+		inferred        = "Hello, I'm a explicitly declared variable"
+	)
+	fmt.Printf("Type: %T Value: %v\n", toBe, toBe)
+	fmt.Printf("Type: %T Value: %v\n", maxInt, maxInt)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
+	fmt.Printf("Type: %T Value: %v\n", explicit, explicit)
+	fmt.Printf("Type: %T Value: %v\n", inferred, inferred)
+	localFunc()
+}
+
+func localFunc() {
+	// example of var block placement can be inside main func scope
+	var (
+		inferred = "Hello"
+	)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+	// This code show undeclare
+	// fmt.Printf("Type: %T Value: %v\n", explicit, explicit)
+	fmt.Printf("Type: %T Value: %v\n", inferred, inferred)
 }
